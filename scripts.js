4be9c6fd8b09d940b28bdfaa0f439393f5c61979
itamartgomes <itamargomesss@gmail.com>
2024-11-05 17:22:56 -0300

    regraDeNegocio

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min + 1) + min)
-    alert(result)
+
+    if (max >= min){
+        const result = Math.floor(Math.random() * (max - min + 1) + min)
+        alert(result)
+    }
+    else {
+        alert("O valor minímo deve ser maior que o valor maximo.")
+    }
+    
 }
